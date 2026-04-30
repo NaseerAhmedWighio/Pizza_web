c6ae556ff90da2ffe24300c7d09468ae4f8de077
NaseerAhmedWighio <naseerahmedwighio@gmail.com>
2026-04-30 22:43:46 +0500

    fixed db

--- api/admin/orders/[id].ts
+++ api/admin/orders/[id].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { prisma } from '../../../lib/db';
+import { prisma } from '@/lib/db';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { id } = req.query;
